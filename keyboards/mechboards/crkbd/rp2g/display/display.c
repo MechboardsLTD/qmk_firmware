2256475a0f7aa17e2896be8f831784c708ea9577
Dasky <32983009+daskygit@users.noreply.github.com>
2025-08-29 14:07:17 +0100

    remove lvgl

--- keyboards/mechboards/crkbd/rp2g/display/display.c
+++ keyboards/mechboards/crkbd/rp2g/display/display.c
@@ -3,13 +3,9 @@
 #include "qp.h"
 #include "qp_comms.h"
 #include "qp_st77xx_opcodes.h"
-#include "gfx/mb.qgf.h"
-#include "qp_lvgl.h"
 #include "printf.h"
 #include "transactions.h"
 #include "screens.h"
-#include "styles.h"
-#include "gfx/ezgif.h"
 
 painter_device_t lcd;
 
@@ -23,19 +19,6 @@ int     currlay      = 0;
 int     lastlay      = 0;
 uint8_t currScreen   = 0xFF;
 
-// button style is going to be no background and no border, with individual buttons on a grey background with padding and rounded edges, mb_col if checked, equally spaced with white text across our 80*25
-
-#ifndef LV_ATTRIBUTE_MEM_ALIGN
-#    define LV_ATTRIBUTE_MEM_ALIGN
-#endif
-
-#ifndef LV_ATTRIBUTE_IMG_MB
-#    define LV_ATTRIBUTE_IMG_MB
-#endif
-
-// LV_FONT_DECLARE(pixellari_18);
-// LV_FONT_DECLARE(pixellari_14);
-
 void display_housekeeping_task(void) {
     // Update WPM every 5 seconds
     if (timer_elapsed(timer) > 1000 && !updating_wpm && (currScreen == 0x00 || currScreen == 0x01)) {
@@ -48,16 +31,12 @@ void display_housekeeping_task(void) {
         if (currwpm == lastwpm) {
             count++;
             if (count < vals) {
-                if (master) { // Disable chart on slave side while bug is resolved.
-                    lv_chart_set_next_value(chart, ser, currwpm);
-                }
+                // lv_chart_set_next_value(chart, ser, currwpm);
             }
         } else {
             count = 0;
-            lv_label_set_text_fmt(label_wpm, "WPM:%d", currwpm);
-            if (master) { // Disable chart on slave side while bug is resolved.
-                lv_chart_set_next_value(chart, ser, currwpm);
-            }
+            // lv_label_set_text_fmt(label_wpm, "WPM:%d", currwpm);
+            // lv_chart_set_next_value(chart, ser, currwpm);
         }
         updating_wpm = false;
     }
@@ -73,7 +52,7 @@ void display_housekeeping_task(void) {
                 case 1:
                 case 2:
                 case 3:
-                    lv_btnmatrix_set_btn_ctrl(layer_btn_matrix, currlay, LV_BTNMATRIX_CTRL_CHECKED);
+                    // lv_btnmatrix_set_btn_ctrl(layer_btn_matrix, currlay, LV_BTNMATRIX_CTRL_CHECKED);
                     break;
             }
         }
@@ -99,43 +78,43 @@ void processes_command(uint8_t *data, uint8_t length) {
         case _TIME:
             read_string(data, string, length - 2);
             uprintf("Time: %s\n", string);
-            lv_label_set_text(label_time, string);
+            // lv_label_set_text(label_time, string);
             break;
         case _RAM:
-            lv_bar_set_value(bar_ram, data[4], LV_ANIM_OFF);
+            // lv_bar_set_value(bar_ram, data[4], LV_ANIM_OFF);
             break;
         case _CPU:
-            lv_bar_set_value(bar_cpu, data[4], LV_ANIM_OFF);
+            // lv_bar_set_value(bar_cpu, data[4], LV_ANIM_OFF);
             break;
         case _GPU:
-            lv_bar_set_value(bar_gpu, data[4], LV_ANIM_OFF);
+            // lv_bar_set_value(bar_gpu, data[4], LV_ANIM_OFF);
             break;
         case _PROGRESS:
-            lv_slider_set_value(progress, data[4], LV_ANIM_ON);
+            // lv_slider_set_value(progress, data[4], LV_ANIM_ON);
             break;
         case _NOWPLAYING:
             read_string(data, string, length - 4);
-            lv_label_set_text_fmt(label_track, "%s", string);
+            // lv_label_set_text_fmt(label_track, "%s", string);
             uprintf("Now Playing: %s\n", string);
             break;
         case _IMAGE: {
             uint16_t x = ((uint16_t)data[4] << 8) | data[5];
             uprintf("Image %d %d %hu %d\n", data[4], data[5], x, data[6]);
-            memcpy(&lv_mb_map[x * 25], &data[7], data[6]);
+            // memcpy(&lv_mb_map[x * 25], &data[7], data[6]);
             break;
         }
         case _IMG_FS: {
             uprintf("Image FS\n");
             uint16_t y = ((uint16_t)data[4] << 8) | data[5];
             uprintf("Image %d %d %hu %d\n", data[4], data[5], y, data[6]);
-            memcpy(&lv_scr[(y * 25)], &data[7], data[6]);
+            // memcpy(&lv_scr[(y * 25)], &data[7], data[6]);
             break;
         }
         case _IMG_GIF: {
             uprintf("Image GIF\n");
             uint16_t z = ((uint16_t)data[4] << 8) | data[5];
             uprintf("GIF %d %d %hu %d\n", data[4], data[5], z, data[6]);
-            memcpy(&ezgif_map[(z * 25)], &data[7], data[6]);
+            // memcpy(&ezgif_map[(z * 25)], &data[7], data[6]);
             break;
         }
         case _STATUS:
@@ -143,16 +122,16 @@ void processes_command(uint8_t *data, uint8_t length) {
             switch (data[4]) {
                 case _IMAGE:
                     uprintf("Image Set\n");
-                    lv_img_set_src(album_art, &mb);
+                    // lv_img_set_src(album_art, &mb);
                     break;
                 case _IMG_FS:
                     uprintf("Image FS Set\n");
-                    lv_img_set_src(img_scr, &scr);
+                    // lv_img_set_src(img_scr, &scr);
                     break;
                 case _IMG_GIF:
                     if (data[5] == 0x00) {
                         uprintf("Gif Pause\n");
-                        lv_obj_del(gif);
+                        // lv_obj_del(gif);
                         draw_screen(0xFF);
                     } else {
                         uprintf("Gif Set %d\n", ((uint16_t)data[6] << 8) | data[7]);
@@ -213,22 +192,10 @@ void display_init(void) {
     qp_comms_stop(lcd);
 #endif
 
-    if (!qp_lvgl_attach(lcd)) return;
-
-    init_styles();
-
-    if (master)
-        draw_screen(0);
-    else
-        draw_screen(0);
+    draw_screen(0);
 }
 
 void draw_screen(uint8_t screen) {
-    lv_obj_clean(lv_scr_act());
-
-    /*Change the active screen's background color*/
-    lv_obj_set_style_bg_color(lv_scr_act(), lv_color_hex(0x000000), LV_PART_MAIN);
-
     updating_wpm = true;
 
     switch (screen) {
@@ -255,5 +222,5 @@ void draw_screen(uint8_t screen) {
     }
     currScreen = screen;
     // Whole Screen Custom Image Container
-    img_scr = lv_img_create(lv_scr_act());
+    // img_scr = lv_img_create(lv_scr_act());
 }
